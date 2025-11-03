182a67dea4975a7264ad58f75bd179e8cb931ad9
Imran Murtala <imranmurtala08@gmail.com>
2025-11-03 09:43:34 +0100

    Fix: use dynamic port and start script
    :wq
    :wq
    :wq

--- date.js
+++ date.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // date.js
 
 exports.getDate = function () {
@@ -21,3 +22,28 @@ exports.getDay = function () {
 
   return today.toLocaleDateString("en-US", options);
 };
+=======
+// date.js
+
+exports.getDate = function () {
+    const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+
+exports.getDay = function () {
+  const today = new Date();
+
+ const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+>>>>>>> 26403c7 (Fix: use dynamic port and start script)
